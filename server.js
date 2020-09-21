450b361109ec8b945c9bf8faaaedc8bd0fd868a0
Edgar Nikoghosyan <edgarnikogosjan@gmail.com>
2020-09-22 01:04:23 +0400

    run build

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
+
+console.log('__dirname', __dirname)
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
 	res.sendFile(path.join(publicPath, 'index.html'));
